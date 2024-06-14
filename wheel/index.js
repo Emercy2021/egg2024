6b538a42100ed4fd177931138b1920a49e9027c1
Emercy2021 <145089445+Emercy2021@users.noreply.github.com>
2024-06-14 16:22:39 +0300

    Update index.js

--- wheel/index.js
+++ wheel/index.js
@@ -361,7 +361,7 @@ p5Instance.onAfterSetup = function () {
 ['videos/12.mp4', 0],
 ['videos/13.mp4', 11],
 ['videos/14.mp4', 4],
-['videos/15.mp4', 15],
+['videos/15.mp4', 17],
 ['videos/17.mp4', 0],
 ['videos/18.mp4', 10],
 ['videos/19.mp4', 0],
