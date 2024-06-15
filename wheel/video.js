863b08026bdf7971a30ae54e3f0fbe960ec6a839
Emercy2021 <145089445+Emercy2021@users.noreply.github.com>
2024-06-15 16:31:09 +0300

    Update video.js

--- wheel/video.js
+++ wheel/video.js
@@ -87,7 +87,7 @@ class Video {
     }
 
     static _shuffle(array) {
-        for (let i = array.length - 1; i > 0; i--) {
+        for (let i = array.length; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1));
             [array[i], array[j]] = [array[j], array[i]];
         }
