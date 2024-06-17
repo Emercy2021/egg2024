9a4791a513fa06c065556824fdd43a1d73d3ce82
Emercy2021 <145089445+Emercy2021@users.noreply.github.com>
2024-06-18 02:46:55 +0300

    Add files via upload

---
+++ wheel/custom.js
@@ -0,0 +1,20 @@
+
+let huey = document.getElementById("huey");
+huey.addEventListener('click', function () {
+    document.getElementById("image").style.borderRadius = "50%" ;
+    }
+);
+
+let dewey = document.getElementById("dewey");
+dewey.addEventListener('click', function () {
+    document.getElementById("image").style.borderRadius = "50%" ;
+    }
+);
+
+
+let scrudge = document.getElementById("scrudge");
+scrudge.addEventListener('click', function () {
+    document.getElementById("image").style.borderRadius = "0%";
+    
+    }
+);
